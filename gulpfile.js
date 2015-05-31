609c98c1a1409e25b10ae4deee0acdcf204351e5
SimonSchick <demwizzy@gmail.com>
2015-05-31 23:42:12 +0200

    Build update

--- gulpfile.js
+++ gulpfile.js
@@ -2,65 +2,77 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var spawn = require('child_process').spawn;
 var Q = require('q');
 
-function validateFiles(files) {
-	return Q.Promise(function(resolve, reject) {
+
+function promisifyStream(stream) {
+	return new Q.Promise(function(resolve, reject) {
+		stream
+		.on('finish', resolve)
+		.on('error', reject);
+	});
+}
+
+function coveralls() {
+	return promisifyStream(
+		gulp.src(['coverage/lcov.info'])
+		.pipe($.coveralls())
+	);
+}
+
+function testAndCoverage(lcovOnly) {
+	var reportOptions;
+
+	if (lcovOnly) {
+		reportOptions = {
+			reporters: ['lcov', 'text', 'text-summary']
+		};
+	}
+
+	return promisifyStream(
+		gulp.src(['test/*.js'])
+		.pipe($.mocha())
+		.pipe($.istanbul.writeReports())
+		.pipe($.istanbul.enforceThresholds({
+			thresholds: {
+				global: 90
+			}
+		}))
+	);
+}
+
+function validateFiles(files, lcovOnly) {
+	return promisifyStream(
 		gulp.src(files)
 		.pipe($.jshint())
 		.pipe($.jshint.reporter(require('jshint-stylish')))
 		.pipe($.jshint.reporter('fail'))
 		.pipe($.jscs())
 		.pipe($.filter(['*', '!test/*']))
-		.pipe($.istanbul()) // Covering files
-		.pipe($.istanbul.hookRequire()) // Force `require` to return covered files
-		.on('finish', function () {
-			gulp.src(['test/*.js'])
-			.pipe($.mocha())
-			.pipe($.istanbul.writeReports()) // Creating the reports after tests runned
-			.pipe($.istanbul.enforceThresholds({
-				thresholds: {
-					global: 90
-				}
-			})) // Enforce a coverage of at least 90%
-			.on('end', resolve)
-			.on('error', reject);
-		})
-		.on('error', reject);
+		.pipe($.istanbul())
+		.pipe($.istanbul.hookRequire())
+	)
+	.then(function() {
+		return testAndCoverage(lcovOnly);
 	});
 }
 
-gulp.task('git-pre-commit', function() {
-	var process = spawn('git', ['diff', '--name-only', '--staged', '--', '*.js']);
+var files = [
+	'**/*.js',
+	'!node_modules/**/*',
+	'!docs/**/*',
+	'!coverage/**/'
+];
 
-	var buffer = new Buffer(0);
-
-	process.stdout.on('data', function(data) {
-		buffer = Buffer.concat([buffer, data]);
-	});
-
-	//this is a tiny hack as Q doesn't seem to supply a method to do this easily...
-	return Q.Promise(function(resolve, reject) {
-		process.on('close', function(code) {
-			try {
-				if (code !== 0) {
-					reject(new Error('Failed to fetch changed files from git'));
-				}
-				var files = buffer.toString().trim().split('\n');
-				resolve(validateFiles(files));
-			} catch (error) {
-				reject(error);
-			}
-		});
-	});
+gulp.task('default', function() {
+	if (process.env.TRAVIS) {
+		return validateFiles(files, true);
+	}
+	return validateFiles(files);
 });
 
-gulp.task('validate', function(cb) {
-	return validateFiles([
-		'**/*.js',
-		'!node_modules/**/*',
-		'!docs/**/*',
-		'!coverage/**/'
-	]);
+gulp.task('coveralls', function() {
+	return coveralls();
 });
+
+gulp.task('git-pre-commit', ['default']);
